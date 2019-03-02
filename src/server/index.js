771afd950ef09007f29c34cca236be119fe4989e
Bartosz Podgruszecki <bartosz.podgruszecki@gmail.com>
2019-03-02 12:37:22 +0100

    Add getTestJson route.

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
-const os = require('os')
+const fs = require('fs')
 
 const app = express()
-
 app.use(express.static('dist'))
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }))
+
+const handleGetTestJson = (req, res) => {
+	return fs.readFile('upload/sftp-server.cfn.json', 'utf8', (err, data) => {
+		if (err) throw err
+		res.send({ filename: 'sftp-server.cfn.json', content: JSON.parse(data) })
+	})
+}
+
+app.get('/api/getUsername', (req, res) => res.send({ username: 'ZMORA' }))
+app.get('/api/getTestJson', handleGetTestJson)
 
 app.listen(process.env.PORT || 8080, () =>
 	console.log(`Listening on port ${process.env.PORT || 8080}!`)
